4d393b6cb28613db626380054a38ef238bbc0e5c
Anthony J Souza <ajsouza88@gmail.com>
2022-02-18 18:41:23 -0800

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       host : '13.57.227.29',
       key  : '~/.ssh/test.pem',
       ref  : 'origin/main',
-      repo : 'https://github.com/asouza88/pm2deploytest',
+      repo : 'git@github.com:asouza88/pm2deploytest.git',
       path : '/var/www/production',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
