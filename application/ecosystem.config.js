531e57535c3941a78420e83d58a1dcbd738defcf
Anthony J Souza <ajsouza88@gmail.com>
2022-02-18 18:25:46 -0800

    t

---
+++ application/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    script: './bin/www',
+  }],
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '13.57.227.29',
+      ref  : 'origin/main',
+      repo : 'git@github.com:asouza88/pm2deploytest.git',
+      path : '/home/ubuntu/',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
